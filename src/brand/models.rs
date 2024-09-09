14930e3ca2d10b06e107e4b8457f6e7dc3db76d9
Maxim Gorin <onlinexmax@gmail.com>
2024-09-09 15:14:58 +0300

    Impl create user

--- src/brand/models.rs
+++ src/brand/models.rs
@@ -2,7 +2,17 @@ use serde::Serialize;
 use serde_derive::Deserialize;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct CreateUserRequest {}
+pub struct CreateUserRequest {
+    pub email: String,
+    pub password: String,
+    #[serde(rename = "firstName")]
+    pub first_name: Option<String>,
+    #[serde(rename = "lastName")]
+    pub last_name: Option<String>,
+}
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct CreateUserResponse {}
+pub struct CreateUserResponse {
+    #[serde(rename = "userId")]
+    pub user_id: String,
+}
