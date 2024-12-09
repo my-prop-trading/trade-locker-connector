4f98db1501428e5a26346c3caf50425482c6694b
Maxim Gorin <onlinexmax@gmail.com>
2024-12-09 17:34:42 +0300

    Add socket io const

--- src/brand_socket/api_client.rs
+++ src/brand_socket/api_client.rs
@@ -1,6 +1,23 @@
 use crate::brand_socket::event_handler::BrandSocketEventHandler;
 use std::sync::Arc;
 
+#[derive(Clone, Debug)]
+struct SocketIoConfig {
+    pub namespace: &'static str,
+    pub handshake_path: &'static str,
+    pub transport: &'static str,
+}
+
+impl Default for SocketIoConfig {
+    fn default() -> Self {
+        Self {
+            namespace: "/brand-socket",
+            handshake_path: "/brand-socket/socket.io",
+            transport: "websocket",
+        }
+    }
+}
+
 pub struct BrandSocketApiClient {
     event_handler: Arc<dyn BrandSocketEventHandler + Send + Sync + 'static>,
 }
