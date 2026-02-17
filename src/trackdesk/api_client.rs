4dd4606083dba0bfbfc26e8ec41a3f7f24ee5666
Troidem <onlinexmax@gmail.com>
2026-02-17 20:40:15 +0300

    feat: add url

--- src/trackdesk/api_client.rs
+++ src/trackdesk/api_client.rs
@@ -1,24 +1,16 @@
+use crate::trackdesk::models::PostbackRequest;
+use flurl::FlUrl;
 use std::time::Duration;
 
-use flurl::FlUrl;
-
-use crate::trackdesk::models::PostbackRequest;
-
-pub struct TrackdeskApiClient {
-    url: String,
-}
+pub struct TrackdeskApiClient {}
 
 impl TrackdeskApiClient {
-    pub fn new(url: impl Into<String>) -> Self {
-        Self { url: url.into() }
-    }
-
     pub async fn postback(&self, request: &PostbackRequest) -> Result<(), String> {
         let query_string = serde_qs::to_string(&request).expect("must be valid model");
-        let url = format!("{}?{}&status=CONVERSION_STATUS_APPROVED", self.url, query_string);
+        let url = format!("https://tradelocker.trackdesk.com/tracking/conversion/v1?status=CONVERSION_STATUS_APPROVED&{}", query_string);
         let flurl = FlUrl::new(&url).set_timeout(Duration::from_secs(15));
         let result = flurl
-            .post(None)
+            .get()
             .await
             .map_err(|e| format!("Failed to send request: {}. Err: {:?}", url, e))?;
         let status_code = result.get_status_code();
